e67bfd1630114c950a331b5209dd809a1303c8a9
Chohanwool <hanwoolc95@gmail.com>
2025-09-22 22:32:47 +0900

    feat: Data Layer 작업 - auth_repository_impl.dart

---
+++ lib/src/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:clean_arch_2/src/features/auth/domain/repositories/auth_repository.dart';
+
+/// AuthRepository 인터페이스 추상메서드 구현 필수!!
+class AuthRepositoryImpl implements AuthRepository {
+  @override
+  Future<bool> login(String email, String password) async {
+    // TODO: Firebase Auth 연동 예정
+    return email == 'test@test.com' && password == '1234';
+  }
+
+  @override
+  Future<bool> register(String email, String password) async {
+    // TODO: Firebase Auth 연동 예정
+    return true;
+  }
+}
